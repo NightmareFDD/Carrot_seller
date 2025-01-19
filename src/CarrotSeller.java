d4504ef509a81b7142a46d4f9ae7f34fa47698cb
Denis <dennis-butenkov@yandex.ru>
2025-01-19 13:19:51 +0100

    Constructor created

--- src/CarrotSeller.java
+++ src/CarrotSeller.java
@@ -1,2 +1,94 @@
 public class CarrotSeller {
+        private String name;
+        private String dateOfBirth;
+        private int numberOfContract;
+        private double amountOfSoldCarrots;
+        private String city;
+        private String address;
+        private String licencePlate;
+        private double consumptionPer100Km;
+
+    // Constructor
+    public CarrotSeller(String name, String dateOfBirth, int numberOfContract, double amountOfSoldCarrots, String city, String address, String licencePlate, double consumptionPer100Km) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.numberOfContract = numberOfContract;
+        this.amountOfSoldCarrots = amountOfSoldCarrots;
+        this.city = city;
+        this.address = address;
+        this.licencePlate = licencePlate;
+        this.consumptionPer100Km = consumptionPer100Km;
+    }
+
+    //Name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Date of birth
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    //Number of contracts
+    public int getNumberOfContract() {
+        return numberOfContract;
+    }
+
+    public void setNumberOfContract(int numberOfContract) {
+        this.numberOfContract = numberOfContract;
+    }
+
+    // Amount of sold Carrots in tons
+    public double getAmountOfSoldCarrots() {
+        return amountOfSoldCarrots;
+    }
+
+    public void setAmountOfSoldCarrots(double amountOfSoldCarrots) {
+        this.amountOfSoldCarrots = amountOfSoldCarrots;
+    }
+
+    // CIty
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    // Address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Licence Plate
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    // Consumption per 100 Km
+    public double getConsumptionPer100Km() {
+        return consumptionPer100Km;
+    }
+
+    public void setConsumptionPer100Km(double consumptionPer100Km) {
+        this.consumptionPer100Km = consumptionPer100Km;
+    }
 }
